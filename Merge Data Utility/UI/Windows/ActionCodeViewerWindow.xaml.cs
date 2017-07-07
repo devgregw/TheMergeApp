d03ecfa22e642925ef26f7da6c4d23024c4b2547
Greg Whatley <devgregw@outlook.com>
2017-07-07 09:52:05 -0500

    Utility: tweaks to ActionCodeViewerWindow

--- Merge Data Utility/UI/Windows/ActionCodeViewerWindow.xaml.cs
+++ Merge Data Utility/UI/Windows/ActionCodeViewerWindow.xaml.cs
@@ -14,23 +14,35 @@ using System.Windows.Shapes;
 using System.Xml;
 using MergeApi.Framework.Abstractions;
 using Newtonsoft.Json;
+using Formatting = Newtonsoft.Json.Formatting;
 
 namespace Merge_Data_Utility.UI.Windows {
     /// <summary>
     /// Interaction logic for ActionCodeViewerWindow.xaml
     /// </summary>
     public partial class ActionCodeViewerWindow : Window {
+        private bool _indent = true, _wrap = false;
+
         public ActionCodeViewerWindow() {
             InitializeComponent();
-            actionField.ShowViewCodeButton = false;
-            actionField.ActionSelected += (s, e) => {
-                var old = codeBox.Text;
-                try {
-                    codeBox.Text = actionField.SelectedAction == null ? "" : JsonConvert.SerializeObject(actionField.SelectedAction, Newtonsoft.Json.Formatting.Indented);
-                } catch {
-                    codeBox.Text = old;
-                }
+            codeBox.TextChanged += (s, e) => {
+                count.Text = codeBox.Text.Length.ToString();
+                tooMany.Visibility = codeBox.Text.Length > 200 ? Visibility.Visible : Visibility.Collapsed;
             };
+            actionField.ShowViewCodeButton = false;
+            actionField.ActionSelected += (s, e) => Update();
+        }
+
+        private void Update() {
+            if (codeBox == null)
+                return;
+            var old = codeBox.Text;
+            codeBox.TextWrapping = _wrap ? TextWrapping.Wrap : TextWrapping.NoWrap;
+            try {
+                codeBox.Text = actionField.SelectedAction == null ? "" : JsonConvert.SerializeObject(actionField.SelectedAction, _indent ? Formatting.Indented : Formatting.None);
+            } catch {
+                codeBox.Text = old;
+            }
         }
 
         public ActionCodeViewerWindow(ActionBase existing) : this() {
@@ -46,6 +58,16 @@ namespace Merge_Data_Utility.UI.Windows {
                 MessageBox.Show($"That code could not be translated into an action.\n{ex.GetType().FullName}: {ex.Message}\n{ex.StackTrace}", "Action Code Viewer",
                     MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
             }
+        }
+
+        private void Indent_Changed(object sender, RoutedEventArgs e) {
+            _indent = ((CheckBox) sender).IsChecked.GetValueOrDefault(true);
+            Update();
+        }
+
+        private void Wrap_Changed(object sender, RoutedEventArgs e) {
+            _wrap = ((CheckBox)sender).IsChecked.GetValueOrDefault(false);
+            Update();
         }
     }
 }
