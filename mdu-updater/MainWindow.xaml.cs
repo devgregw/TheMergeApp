be378ce1f1f44b59bbc9b3cd212c3745dc10d626
Greg Whatley <devgregw@outlook.com>
2017-07-18 17:37:32 -0500

    Updater: code cleanup (ReSharper)

--- mdu-updater/MainWindow.xaml.cs
+++ mdu-updater/MainWindow.xaml.cs
@@ -1,25 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
+﻿#region LICENSE
+
+// Project mdu-updater:  MainWindow.xaml.cs (in Solution mdu-updater)
+// Created by Greg Whatley on 06/24/2017 at 10:57 AM.
+// 
+// The MIT License (MIT)
+// 
+// Copyright (c) 2017 Greg Whatley
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#endregion
+
+#region USINGS
+
+using System;
 using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
+#endregion
 
 namespace mdu_updater {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
         public MainWindow() {
@@ -30,10 +51,9 @@ namespace mdu_updater {
                         status.Text = "Applying update...";
                         progress.IsIndeterminate = true;
                         using (var archive = ZipFile.OpenRead("update.zip")) {
-                            foreach (var file in archive.Entries) {
+                            foreach (var file in archive.Entries)
                                 if (File.Exists(file.FullName))
                                     File.Delete(file.FullName);
-                            }
                         }
                         ZipFile.ExtractToDirectory("update.zip", Environment.CurrentDirectory);
                         status.Text = "Complete.";
@@ -47,7 +67,8 @@ namespace mdu_updater {
                         Close();
                     };
                     client.DownloadProgressChanged += (ss, ee) => {
-                        status.Text = $"Downloading update...\n{ee.BytesReceived} bytes received.\n{ee.TotalBytesToReceive - ee.BytesReceived} bytes remaining.";
+                        status.Text =
+                            $"Downloading update...\n{ee.BytesReceived} bytes received.\n{ee.TotalBytesToReceive - ee.BytesReceived} bytes remaining.";
                         progress.IsIndeterminate = false;
                         progress.Value = ee.ProgressPercentage;
                     };
