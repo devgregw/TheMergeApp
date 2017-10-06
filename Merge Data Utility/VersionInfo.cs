f918fa82386f876b8b53665d5a893b8e9cfefaa8
Greg Whatley <devgregw@outlook.com>
2017-10-05 19:35:55 -0500

    Utility: use new Merge Core

--- Merge Data Utility/VersionInfo.cs
+++ Merge Data Utility/VersionInfo.cs
@@ -35,10 +35,10 @@ using System;
 
 namespace Merge_Data_Utility {
     public static class VersionInfo {
-        public const string VersionString = "2.0.1.6";
+        public const string VersionString = "2.0.1.7";
 
         public static readonly Version Version = Version.Parse(VersionString);
 
-        public static readonly int Update = 22;
+        public static readonly int Update = 23;
     }
 }
